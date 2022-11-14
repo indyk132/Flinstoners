3efadb51958516baca7b747210a3d852331897c3
Krystian <krystianszub@wp.pl>
2022-11-14 12:20:21 +0100

    #3 - Poprawa estetyczna menu hamburgerowego

--- js/pruductsHAMBURGER.js
+++ js/pruductsHAMBURGER.js
@@ -1,6 +1,6 @@
 const products = document.querySelector('.hamburger__productsMenu');
 const expander = document.querySelector('.expander__arrow')
 function PRODUCTSmenu(){
-    hamburger__productsMenu.classList.toggle('onclick-switch')
+    products.classList.toggle('onclick-switch')
 }
 expander.addEventListener('click', PRODUCTSmenu)
