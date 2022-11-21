0128f38ccfec5a109f596dad773f0b5ab8add9c3
Stefan <indyks@gmail.com>
2022-11-21 13:10:44 +0100

    #7 - animacja

--- js/productsSlider.js
+++ js/productsSlider.js
@@ -1,16 +1,27 @@
-const leftArrow = document.querySelector('.button__leftArrow');
-const rightArrow = document.querySelector('.button__rightArrow');
-const productsSliderBulbs = document.querySelector('.products__sliderBulbs')
-const bulb__Move = document.querySelectorAll('.slide__bulbMove');
-const bulbContainer = document.querySelector('.products__sliderBulbContainer');
+const leftArrow = document.querySelector(".button__leftArrow");
+const rightArrow = document.querySelector(".button__rightArrow");
+const productsSliderBulbs = document.querySelectorAll(".products__sliderBulbs");
+const bulb__Move = document.querySelectorAll(".slide__bulbMove");
+const bulbContainer = document.querySelector(".products__sliderBulbContainer");
+let  move = 0
 
-const swapperLeft = () => { 
-    productsSliderBulbs.classList.toggle("bulb__mover--left");
-    // bulbContainer.firstChild.add(bulb5)
-    // bulbContainer.lastChild.remove(bulb5);
-}
-const swapperRight = () => { 
-        productsSliderBulbs.classList.toggle("bulb__mover--right");
-}
-rightArrow.addEventListener('click', swapperRight);
-leftArrow.addEventListener('click', swapperLeft);
+const swapperLeft = () => {
+    for (let i  = 0; i < bulb__Move.length; i++){
+        bulb__Move[i].style.transform=`translateX(${move - 100 + "px"})`;
+    }
+    move -= 100
+};
+const swapperRight = () => {
+    for (let i  = 0; i < bulb__Move.length; i++){
+        bulb__Move[i].style.transform=`translateX(${move + 100 + "px"})`
+    }
+    move += 100
+
+    rightArrow.style ="z-index: 1000"
+};
+productsSliderBulbs.forEach((e) => {
+	rightArrow.addEventListener("click", swapperRight);
+});
+productsSliderBulbs.forEach((e) => {
+	leftArrow.addEventListener("click", swapperLeft);
+});
