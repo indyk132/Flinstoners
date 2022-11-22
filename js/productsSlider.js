28229ef252d2c6792a09ffa49cedfe6c71dabb22
Stefan <indyks@gmail.com>
2022-11-22 11:48:15 +0100

    7 - przesuwanie slidera

--- js/productsSlider.js
+++ js/productsSlider.js
@@ -1,27 +1,28 @@
 const leftArrow = document.querySelector(".button__leftArrow");
 const rightArrow = document.querySelector(".button__rightArrow");
-const productsSliderBulbs = document.querySelectorAll(".products__sliderBulbs");
-const bulb__Move = document.querySelectorAll(".slide__bulbMove");
+const  productsSliderScroll = document.querySelector(".product__sliderScroll");
+const bulb__Move = document.querySelectorAll(".slide__bulbMove").length;
 const bulbContainer = document.querySelector(".products__sliderBulbContainer");
-let  move = 0
+const maxMove = bulb__Move;
+let  swapperMove = 0
 
-const swapperLeft = () => {
-    for (let i  = 0; i < bulb__Move.length; i++){
-        bulb__Move[i].style.transform=`translateX(${move - 100 + "px"})`;
-    }
-    move -= 100
-};
-const swapperRight = () => {
-    for (let i  = 0; i < bulb__Move.length; i++){
-        bulb__Move[i].style.transform=`translateX(${move + 100 + "px"})`
-    }
-    move += 100
+const swapper = function(direction) {
+    const productsSliderBulbs = document.querySelector(".products__sliderBulbs").offsetWidth;
+    const width = bulbContainer.offsetWidth + 10;
+    let visibleCount = Math.round(productsSliderBulbs / width);
+    let visbleSum = maxMove - visibleCount;
+    
+    if (direction === "left"){
+            if (swapperMove > 0){
+                swapperMove -= 1;
+            }
+    } else  if(direction ==="right"){
+        if (swapperMove >= 0 & swapperMove < visbleSum){
+                swapperMove += 1;
+            }
+        } else return;
+        productsSliderScroll.style.transform=`translateX(${"-" + swapperMove * width + "px"})`;
+}
 
-    rightArrow.style ="z-index: 1000"
-};
-productsSliderBulbs.forEach((e) => {
-	rightArrow.addEventListener("click", swapperRight);
-});
-productsSliderBulbs.forEach((e) => {
-	leftArrow.addEventListener("click", swapperLeft);
-});
+rightArrow.addEventListener("click", () => {swapper("right")});
+leftArrow.addEventListener("click", () =>{swapper("left")} );
